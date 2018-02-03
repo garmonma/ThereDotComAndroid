7523e9605d1f4d13a83f42fa94937b61d238eba6
garmonma <garmonmarcus@gmail.com>
2018-02-03 14:28:50 -0700

    Created Fragments for the calculator.

--- app/src/main/java/android/nni/com/theredotcomandroid/MainActivity.kt
+++ app/src/main/java/android/nni/com/theredotcomandroid/MainActivity.kt
@@ -58,8 +58,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
+            R.id.nav_calculator -> {
+
             }
             R.id.nav_gallery -> {
 
